c06cd1d828d9b2795e36e18634ec1dbd31188659
Anoushkrit Goel <anoushkritgoel@gmail.com>
2024-03-18 09:58:10 +0530

    minor update

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env sh
 HOME=`pwd`
 
-# Chamfer Distance
+# export $CUDA_PATH=/usr/local/cuda
+# export $CUDA_HOME=/usr/local/cuda
+# cd /usr/local/cuda
+# Chamfer Distancepwd
 cd $HOME/extensions/chamfer_dist
 python setup.py install --user
 
